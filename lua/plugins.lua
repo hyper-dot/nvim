78cb4195c1676578a91767e68f4919c2125b7ba7
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-25 14:44:50 +0545

    added vim sorround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -105,4 +105,14 @@ require("lazy").setup({
 			require("Comment").setup()
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 })
