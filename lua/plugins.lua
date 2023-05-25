7a7959c95470a243f61a30ddaabf9a011f20b48c
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-25 13:48:59 +0545

    formatted

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,57 +15,81 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-   "nvim-lua/plenary.nvim", -- required by many packages
+	"nvim-lua/plenary.nvim", -- required by many packages
 
-   -- Vscode colorscheme
-   {
-      "Mofiqul/vscode.nvim",
-      lazy = false,
-      config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme vscode]])
-      end,
-   },
-   -- Devicons
-   "nvim-tree/nvim-web-devicons",
+	-- Vscode colorscheme
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme vscode]])
+		end,
+	},
+	-- Devicons
+	"nvim-tree/nvim-web-devicons",
 
-   --Buffer line that shows tabs
-   "akinsho/bufferline.nvim",
+	--Buffer line that shows tabs
+	"akinsho/bufferline.nvim",
 
-   -- Colorizer that colorize hash colors
-   "norcalli/nvim-colorizer.lua",
+	-- Colorizer that colorize hash colors
+	"norcalli/nvim-colorizer.lua",
 
-   -- File explorer
-   {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
-      dependencies = {
-         "nvim-lua/plenary.nvim",
-         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-         "MunifTanjim/nui.nvim",
-      },
-   },
-   -- Lua line status line
-   {
-      "nvim-lualine/lualine.nvim",
-      requires = { "nvim-tree/nvim-web-devicons", opt = true },
-      config = function ()
-        require('lualine').setup()
-      end
-   },
+	-- File explorer
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	-- Lua line status line
+	{
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
 
-   -- fuzzy finding w/ telescope
-   { "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
-   {
-    'glepnir/dashboard-nvim',
-     event = 'VimEnter',
-   },
-    {
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
-    {'nvim-treesitter/nvim-treesitter'},
+	-- fuzzy finding w/ telescope
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
+	{
+		"glepnir/dashboard-nvim",
+		event = "VimEnter",
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+
+	-- Auto completion
+	"hrsh7th/nvim-cmp", -- auto completion plugin
+	"hrsh7th/cmp-buffer", -- source for nvim cmp
+	"hrsh7th/cmp-path", -- for path autocompletion
+
+	-- Snippets
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+
+	-- Managing and installing lsp server
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+
+	-- Configuring lsp server
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	{ "glepnir/lspsaga.nvim", branch = "main" },
+	"jose-elias-alvarez/typescript.nvim",
+	"onsails/lspkind.nvim",
+
+	-- Formating and linting
+	"jose-elias-alvarez/null-ls.nvim",
 })
