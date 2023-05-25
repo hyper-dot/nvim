61c203643e40e9ed9a270688952bcebdd888f793
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-25 13:47:48 +0545

    minor changes

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
--- Core 
+-- Core
 require("core.base")
 require("core.keymappings")
 
 require("plugins")
+
+-- plugins
 require("plugin_configs.colorizer")
 require("plugin_configs.bufferline")
 require("plugin_configs.neotree")
@@ -10,7 +12,8 @@ require("plugin_configs.telescope")
 require("plugin_configs.dashboard")
 require("plugin_configs.treesitter")
 require("plugin_configs.lualine")
-
-
-
-
+require("plugin_configs.lsp.mason")
+require("plugin_configs.lsp.lspsaga")
+require("plugin_configs.lsp.lspconfig")
+require("plugin_configs.lsp.null-ls")
+require("plugin_configs.nvim-cmp")
