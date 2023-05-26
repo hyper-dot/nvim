014d0edfe9fd4af10eba2ed67954d5ac9602dff3
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-26 16:25:29 +0545

    modified icons

--- lua/plugin_configs/neotree.lua
+++ lua/plugin_configs/neotree.lua
@@ -52,14 +52,14 @@ require("neo-tree").setup({
 		git_status = {
 			symbols = {
 				-- Change type
-				added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-				modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+				added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+				modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
 				deleted = "✖", -- this can only be used in the git_status source
 				renamed = "R", -- this can only be used in the git_status source
 				-- Status type
-				untracked = "",
+				untracked = "",
 				ignored = "",
-				unstaged = "",
+				unstaged = "",
 				staged = "",
 				conflict = "",
 			},
