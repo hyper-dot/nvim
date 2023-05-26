268457c75d35eb6958c563f63a927e02cb778baa
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-26 16:25:46 +0545

    modified tab and shift width

--- lua/core/base.lua
+++ lua/core/base.lua
@@ -7,6 +7,7 @@ vim.opt.fileencoding = "utf-8"
 vim.opt.clipboard = "unnamedplus"
 
 vim.wo.number = true
+vim.opt.relativenumber = true
 
 vim.opt.title = true
 vim.opt.autoindent = true
@@ -25,8 +26,8 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { "start", "eol", "indent" }
 
