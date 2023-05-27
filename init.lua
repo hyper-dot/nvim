c3a549664a435a879de252c42844f14081d3cf43
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-27 08:04:26 +0545

    organized files

--- init.lua
+++ init.lua
@@ -3,17 +3,23 @@ require("core.base")
 require("core.keymappings")
 require("plugins")
 
--- plugins
-require("plugin_configs.colorizer")
-require("plugin_configs.bufferline")
-require("plugin_configs.neotree")
-require("plugin_configs.telescope")
-require("plugin_configs.dashboard")
-require("plugin_configs.treesitter")
-require("plugin_configs.lualine")
+-- Plugins
+
+-- LSP
 require("plugin_configs.lsp.mason")
 require("plugin_configs.lsp.lspsaga")
 require("plugin_configs.lsp.lspconfig")
 require("plugin_configs.lsp.null-ls")
 require("plugin_configs.nvim-cmp")
+
+-- UI
+require("plugin_configs.ui.neotree")
+require("plugin_configs.ui.dashboard")
+require("plugin_configs.ui.colorizer")
+require("plugin_configs.ui.bufferline")
+require("plugin_configs.ui.treesitter")
+require("plugin_configs.ui.lualine")
+
+-- UTILS
+require("plugin_configs.telescope")
 require("plugin_configs.autopairs")
