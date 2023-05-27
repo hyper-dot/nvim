1b913a53ce09f9d25c2303876d4555a22e7fa007
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-27 07:50:19 +0545

    markdown-preview plugin added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -115,4 +115,16 @@ require("lazy").setup({
 			})
 		end,
 	},
+	-- Markdown plugin
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g["mkdp_theme"] = "light"
+			vim.g["mkdp_browser"] = "firefox"
+			vim.g["mkdp_auto_close"] = 0
+		end,
+		ft = { "markdown" },
+	},
 })
