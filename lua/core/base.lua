4e28bd3a83aa7e9d1241f82f6e4e2a6f14257e33
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-29 14:09:52 +0545

    tab width updated to 2

--- lua/core/base.lua
+++ lua/core/base.lua
@@ -28,8 +28,8 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { "start", "eol", "indent" }
 
