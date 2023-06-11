65c877effd43b5448fbab08ca66f497d60fa0bb5
Roshan Paudel <rozanpoudel@gmail.com>
2023-06-11 16:58:11 +0545

    removed irritating lightbulb

--- lua/plugin_configs/lsp/lspsaga.lua
+++ lua/plugin_configs/lsp/lspsaga.lua
@@ -1,18 +1,24 @@
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
 saga.setup({
-  -- keybinds for navigation in lspsaga window
-  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-  -- use enter to open file with definition preview
-  definition = {
-    edit = "<CR>",
-  },
---  ui = {
---    colors = {
---      normal_bg = "#022746",
---    },
---  },
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+	-- use enter to open file with definition preview
+	definition = {
+		edit = "<CR>",
+	},
+	--  ui = {
+	--    colors = {
+	--      normal_bg = "#022746",
+	--    },
+	--  },
+	lightbulb = {
+		enable = false,
+		enable_in_insert = false,
+		sign = false,
+		virtual_text = false,
+	},
 })
