520e4f499955207cbf5b799757ef507a12eb08b9
Roshan Paudel <rozanpoudel@gmail.com>
2023-06-11 16:58:01 +0545

    removed diagnostics sign

--- lua/plugin_configs/lsp/diagnostics.lua
+++ lua/plugin_configs/lsp/diagnostics.lua
@@ -1,5 +1,6 @@
 -- Icons for diagnostics
-local signs = { Error = "", Warn = "", Hint = "💡", Info = "🔥" }
+-- local signs = { Error = "", Warn = "", Hint = "💡", Info = "🔥" }
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
