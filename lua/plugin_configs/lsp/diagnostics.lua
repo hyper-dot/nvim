06e3a6277b7b1b0f7500b2453f1d8db3d70b7d9c
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-11 23:15:11 +0545

    added all configs again

---
+++ lua/plugin_configs/lsp/diagnostics.lua
@@ -0,0 +1,24 @@
+-- Icons for diagnostics
+-- local signs = { Error = "", Warn = "", Hint = "💡", Info = "🔥" }
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+	},
+
+	update_in_insert = false,
+	float = {
+		source = "always", --Or 'if_many'
+	},
+})
+
+-- Disable virtual text for diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
+})
