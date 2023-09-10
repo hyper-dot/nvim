f76b50cbc32b5f8fcaa846bd223ccc24046694e9
Roshan Paudel <rozanpoudel@gmail.com>
2023-09-10 15:58:38 +0545

    colorizer changed

--- lua/roshan/plugins/init.lua
+++ lua/roshan/plugins/init.lua
@@ -4,4 +4,6 @@ return {
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+
+  "norcalli/nvim-colorizer.lua", -- colorizer
 }
