d209ef606b6c1f0488d28127d778429647f04416
Roshan Paudel <rozanpoudel@gmail.com>
2023-12-19 22:05:43 +0545

    gitsigns configured

--- lua/josean/plugins/gitsigns.lua
+++ lua/josean/plugins/gitsigns.lua
@@ -1,5 +1,9 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("gitsigns").setup()
+		local key = vim.keymap
+		key.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "show git blame" })
+	end,
 }
