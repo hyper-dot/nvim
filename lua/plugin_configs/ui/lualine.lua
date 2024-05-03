20470364a199cdb4b10852598114e70d84e2839c
Roshan Paudel <rozanpoudel@gmail.com>
2024-05-03 21:39:11 +0545

    🔧 CHORE: minor configs

--- lua/plugin_configs/ui/lualine.lua
+++ lua/plugin_configs/ui/lualine.lua
@@ -7,7 +7,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = auto,
+		theme = 'auto',
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
